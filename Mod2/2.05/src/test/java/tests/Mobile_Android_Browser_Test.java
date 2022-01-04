cb0e526e24817c3a8ee5eac33ebf910c50e1b551
Ann <amillikin@us.globalinfotek.com>
2022-01-04 12:14:37 -0500

    2.05 changes - Run an Emulator Web Browser Test on Sauce Labs

--- Mod2/2.05/src/test/java/tests/Mobile_Android_Browser_Test.java
+++ Mod2/2.05/src/test/java/tests/Mobile_Android_Browser_Test.java
@@ -1,104 +1,113 @@
-package tests;
+    package tests;
 
-import io.appium.java_client.MobileBy;
-import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.By;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
-import java.net.URL;
-
-
-public class Mobile_Android_Browser_Test {
-
-    private static final String APP = "/Users/lindsaywalker/Documents/Example_Tests/Android.SauceLabs.Mobile.Sample.app.2.7.0.apk";
-    private static final String APPIUM = "http://localhost:4723/wd/hub";
-
-    private AndroidDriver driver;
-
-    String usernameID = "test-Username";
-    String passwordID = "test-Password";
-    String submitButtonID = "test-LOGIN";
-    By ProductTitle = By.xpath("//android.widget.TextView[@text='PRODUCTS']");
-
-    @BeforeMethod
-    public void setUp() throws Exception {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("deviceName", "Android Emulator"); //This will change to type of device e.g. Pixel 4 on Saucelabs
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion","9.0" ); //add platformVersion
-        capabilities.setCapability("automationName", "UiAutomator2");
-        capabilities.setCapability("appWaitActivity", "com.swaglabsmobileapp.MainActivity");
-        capabilities.setCapability("app", APP);
+    import io.appium.java_client.MobileBy;
+    import io.appium.java_client.android.AndroidDriver;
+    import org.openqa.selenium.By;
+    import org.openqa.selenium.TimeoutException;
+    import org.openqa.selenium.WebElement;
+    import org.openqa.selenium.remote.DesiredCapabilities;
+    import org.openqa.selenium.support.ui.ExpectedConditions;
+    import org.openqa.selenium.support.ui.WebDriverWait;
+    import org.testng.Assert;
+    import org.testng.annotations.AfterMethod;
+    import org.testng.annotations.BeforeMethod;
+    import org.testng.annotations.Test;
+    import static tests.Config.region; // added
+    import java.net.URL;
 
 
-        driver = new AndroidDriver(new URL(APPIUM), capabilities);
+    public class Mobile_Android_Browser_Test {
+        URL url;
+        // Deleted APP and APPIUM
+        String appUrl = "https://www.saucedemo.com/"; //added
 
-    }
+        private AndroidDriver driver;
 
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
+    //    String usernameID = "test-Username";
+    //    String passwordID = "test-Password";
+    //    String submitButtonID = "test-LOGIN";
+    //    By ProductTitle = By.xpath("//android.widget.TextView[@text='PRODUCTS']");
+        By usernameInput = By.id("user-name");
+        By passwordInput = By.id("password");
+        By submitButton = By.className("btn_action");
+        By productTitle = By.className("product_label");
+
+
+        @BeforeMethod
+        public void setUp() throws Exception {
+            System.out.println("Sauce Android Mobile Browser EMU Test - AfterMethod Hook"); //added
+            String username = System.getenv("SAUCE_USERNAME"); // added
+            String accesskey = System.getenv("SAUCE_ACCESS_KEY"); //added
+            String sauceUrl;
+            if (region.equalsIgnoreCase("eu")) {
+                sauceUrl = "@ondemand.eu-central-1.saucelabs.com:443";
+            } else {
+                sauceUrl = "@ondemand.us-west-1.saucelabs.com:443";
+            }
+            String SAUCE_REMOTE_URL = "https://" + username + ":" + accesskey + sauceUrl + "/wd/hub"; // if else statement added
+            url = new URL(SAUCE_REMOTE_URL); //added
+            System.out.println("url: "+ url);
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("deviceName", "Android Emulator"); // added
+            capabilities.setCapability("platformName", "Android");
+            capabilities.setCapability("platformVersion","8.0" );
+            capabilities.setCapability("automationName", "UiAutomator2");
+    //        capabilities.setCapability("appWaitActivity", "com.swaglabsmobileapp.MainActivity"); //removed
+            capabilities.setCapability("browserName", "Chrome");// added
+    //        driver = new AndroidDriver(new URL(APPIUM), capabilities); //removed
+            driver = new AndroidDriver(url, capabilities); //added
         }
 
-    }
+        @AfterMethod
+        public void tearDown() {
+            System.out.println("Sauce Android Mobile Browser EMU Test - AfterMethod Hook");// added
+            if (driver != null) {
+                driver.quit();
+            }
 
-    @Test
-    public void loginToSwagLabsTestValid() {
-        System.out.println("Sauce - Start loginToSwagLabsTestValid test");
-
-        login("standard_user", "secret_sauce");
-
-        // Verification
-        Assert.assertTrue(isOnProductsPage());
-    }
-
-    @Test
-    public void loginTestValidProblem() {
-        System.out.println("Sauce - Start loginTestValidProblem test");
-
-        login("problem_user", "secret_sauce");
-
-        // Verification - we on Product page
-        Assert.assertTrue(isOnProductsPage());
-    }
-
-    public void login(String user, String pass){
-
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        final WebElement usernameEdit = wait.until(ExpectedConditions.visibilityOfElementLocated(new MobileBy.ByAccessibilityId(usernameID)));
-
-            usernameEdit.click();
-            usernameEdit.sendKeys(user);
-
-        WebElement passwordEdit = driver.findElementByAccessibilityId(passwordID);
-            passwordEdit.click();
-            passwordEdit.sendKeys(pass);
-
-        WebElement submitButton = driver.findElementByAccessibilityId(submitButtonID);
-            submitButton.click();
-    }
-
-    public boolean isOnProductsPage() {
-        //Create an instance of a Appium explicit wait so that we can dynamically wait for an element
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-
-        //wait for the product field to be visible and store that element into a variable
-        try {
-            wait.until(ExpectedConditions.visibilityOfElementLocated(ProductTitle));
-        } catch (TimeoutException e){
-            System.out.println("*** Timed out waiting for product page to load.");
-            return false;
         }
-        return true;
+
+        @Test
+        public void loginToSwagLabsTestValid() throws Exception {
+            System.out.println("Sauce - Start loginToSwagLabsTestValid test");
+
+            login("standard_user", "secret_sauce");
+            try {
+                Thread.sleep(10000);
+            } catch (java.lang.Exception e) {
+                e.printStackTrace();
+            }
+
+            // Verification
+            Assert.assertTrue(isOnProductsPage());
+        }
+
+        @Test
+        public void loginTestValidProblem() throws Exception {
+            System.out.println("Sauce - Start loginTestValidProblem test");
+
+            login("problem_user", "secret_sauce");
+            try {
+                Thread.sleep(10000);
+            } catch (java.lang.Exception e) {
+                e.printStackTrace();
+            }
+
+            // Verification - we on Product page
+            Assert.assertTrue(isOnProductsPage());
+        }
+
+        public void login(String user, String pass){
+            driver.get(appUrl);
+            driver.findElement(usernameInput).sendKeys(user);
+            driver.findElement(passwordInput).sendKeys(pass);
+
+            driver.findElement(submitButton).click();
+
+        }
+
+        public boolean isOnProductsPage() {
+            return driver.findElement(productTitle).isDisplayed();
+        }
     }
-}
 
